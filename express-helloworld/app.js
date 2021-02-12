a5fe9395bf9342c3c1c205f0f69c30c78f5eef96
Sandman133 <77737329+Sandman133@users.noreply.github.com>
2021-02-12 14:15:48 +0100

    Fixed Typo

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('This is Verion3 of the app');
+  res.send('This is Verion 3 of the app');
 });
 
 app.listen(8080, function () {
