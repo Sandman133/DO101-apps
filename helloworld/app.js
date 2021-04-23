ec11a4dac53c0c756ffe2c2ef88bb533446497b9
Jan Woellner <jan.woellner@schufa.de>
2021-04-23 10:54:25 +0200

    New Project created

---
+++ helloworld/app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('This is Verion 3 of the app');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
